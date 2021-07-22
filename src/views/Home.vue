4312c149bfb2386b95c89a5bbf2f54a77c2c39ac
exvass31 <admin@MacBook-Pro-de-admin.local>
2021-07-22 09:23:18 -0400

    avanzando

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <px-assets-table :assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+import api from '@/api.js'
+
+export default {
+  name: "Home",
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: [],
+    }
+  },
+
+  created () {
+    api.getAssets().then(assets => (this.assets = assets))
+  },
+};
+</script>
